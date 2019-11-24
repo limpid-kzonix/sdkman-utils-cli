63971c4fd48b123ade18de4ce531e06cb55565c8
Alexander Balyshyn <balyszyn@gmail.com>
2019-11-24 23:00:51 +0200

    Initial dependencies
    Co-authored-by: Alexander Balyshyn <balyszyn@gmail.com>

--- src/main.cr
+++ src/main.cr
@@ -1,6 +1,13 @@
 # TODO: Write documentation for `./`
-module ./
+module Main
   VERSION = "0.1.0"
 
+  # A unicorn is a **legendary animal** (see the `Legendary` module) that has been
+  # described since antiquity as a beast with a large, spiraling horn projecting
+  # from its forehead.
+  class Unicorn
+    def initialize
+    end
+  end
   # TODO: Put your code here
 end
